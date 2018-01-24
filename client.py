acc2740d0d33905d23195a17da54f54f1f340b43
Tyler Eakes <teakesprofessional@gmail.com>
2018-01-24 11:24:39 -0800

    removed depricated

--- client.py
+++
@@ -1,6 +0,0 @@
-
-client_id = '11Uowqt1hKqYATeNUFyfC6XPecHQDVk78gQS0JuG'
-
-client_secret = 'dyYFIaVEkDJg7iYNSGOBkMs65sWHQLrHz7NC2nFuzsrJVg6YeOi2ekLOk60pV4D9ohGUGQDyz5khGJI0LWpQ8yxghRUwKvwniTsHkDrll4ITLaZzXylXQpAoukHpeRMD'
-
-callback_url = 'http://127.0.0.1:5000/callback'
