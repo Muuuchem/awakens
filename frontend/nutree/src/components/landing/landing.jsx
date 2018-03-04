c282f4d1dd7553839dbfac984486ec7ed69eb16f
Younis Hamalawy <younis.hamalawy@gmail.com>
2018-03-03 20:02:23 -0800

    background image for landing page

--- frontend/nutree/src/components/landing/landing.jsx
+++ frontend/nutree/src/components/landing/landing.jsx
@@ -14,27 +14,24 @@ class LandingPage extends React.Component {
       <div>
         <section className="style1" id="intro">
           <div className="intro-content">
-            <div className="intro-inner-content">
-              <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-              <h3>
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam
-              </h3>
-              <ul className="intro">
-                <li>
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </li>
-                <li>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                  fugiat nulla pariatur.
-                </li>
-                <li>
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </li>
-              </ul>
-            </div>
-            <img alt="tree" src={require('../../images/tree.jpg')} />
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            <h3>
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam
+            </h3>
+            <ul className="intro">
+              <li>
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </li>
+              <li>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.
+              </li>
+              <li>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+              </li>
+            </ul>
           </div>
         </section>
 
