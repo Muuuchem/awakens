43b6aedb885a5ddb92b563c7d7564b46da3f369e
Younis Hamalawy <younis.hamalawy@gmail.com>
2018-03-03 18:28:28 -0800

    basic styling landing page

--- frontend/nutree/src/App.js
+++ frontend/nutree/src/App.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Canvas from './components/canvas/canvas';
 import SessionFormContainer from './components/login/login_container';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './components/home/home';
 import Header from './components/header/header';
+import LandingPage from './components/landing/landing';
 import { AuthRoute, ProtectedRoute } from './actions/route_actions';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="main-back">
-      <Header />
-      <Switch>  
-        <ProtectedRoute exact path="/" component={HomePage} />
-        <ProtectedRoute exact path="/home" component={HomePage} />
-        <AuthRoute path="/login" component={SessionFormContainer} />
-        <AuthRoute path="/signup" component={SessionFormContainer} />
-      </Switch>
-    </div>
-  );
-};
+const App = () => (
+  <div className="main-back">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <ProtectedRoute exact path="/home" component={HomePage} />
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+      <Redirect to="/" />
+    </Switch>
+  </div>
+);
 
 export default App;
--- frontend/nutree/src/components/landing/landing.js
+++ frontend/nutree/src/components/landing/landing.js
@@ -4,20 +4,65 @@ import { HashRouter, Switch, Link, Redirect } from 'react-router-dom';
 // import RecipeMatches from '../vitamin/recipe_matches';
 
 class LandingPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          
-        };
-        
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <div>
+        <section className="style1" id="intro">
+          <div className="intro-content">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+            <h3>
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam
+            </h3>
+            <ul className="intro">
+              <li>
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </li>
+              <li>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.
+              </li>
+              <li>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+              </li>
+            </ul>
+          </div>
+        </section>
 
-    render() {
-        return (
-            this
-        );
-    }
+        <section className="style2" id="one">
+          <div className="one-content">
+            <header>
+              <h2>The sky is the limit.</h2>
+            </header>
+            <p className="one">
+              Some resources claim that 'the sky's the limit' was coined by Cervantes in Don
+              Quizote. This appears to add to the list of popular fallacies about coinages
+              attributed to Cervantes; for example, 'don’t put all your eggs in one basket'.
+            </p>
+          </div>
+        </section>
+
+        <section className="style3" id="two">
+          <div className="two-content">
+            <header>
+              <h2>The sky is the limit.</h2>
+            </header>
+            <p className="two">
+              Some resources claim that 'the sky's the limit' was coined by Cervantes in Don
+              Quizote. This appears to add to the list of popular fallacies about coinages
+              attributed to Cervantes; for example, 'don’t put all your eggs in one basket'.
+            </p>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
