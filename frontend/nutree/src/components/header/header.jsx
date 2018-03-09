b972433c4bf683b6ff34a73d2cfdf99937ffde11
Younis Hamalawy <younis.hamalawy@gmail.com>
2018-03-08 20:24:32 -0800

    add 'edmam badge' and miron adjustments to header

--- frontend/nutree/src/components/header/header.jsx
+++ frontend/nutree/src/components/header/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import GreetingContainer from './greeting_container';
+
 class Header extends React.Component {
   render() {
     return (
       <header className="App-header">
-        <h1 className="App-title">NuTree</h1>
-        <GreetingContainer/>
+        <div className="App-title">
+          <h1>NuTree</h1>
+          <div id="edamam-badge" data-color="transparent" />
+        </div>
+        <GreetingContainer />
       </header>
     );
   }
