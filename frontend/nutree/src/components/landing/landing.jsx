5c03e9e2c5709a001666780b05750403087b4613
Younis Hamalawy <younis.hamalawy@gmail.com>
2018-03-17 13:34:20 -0700

    navbar styling and middle nav on main page

--- frontend/nutree/src/components/landing/landing.jsx
+++ frontend/nutree/src/components/landing/landing.jsx
@@ -32,114 +32,58 @@ class LandingPage extends React.Component {
             </li>
           </ul>
         </nav>
-        <section className="style2" id="two">
+        <section className="style 2" id="overview">
           <div className="two-content">
             <h1>
                 Nutree Overview
             </h1>
             <ul className="intro">
-
               <li>NuTree connects with your genomeLink account to access nutrional genomic data.</li>
-
-              <li>
-                Using your personal nutrional data, NuTree suggests recipes with high levels of the micronutrients your body craves most, based on predictative factors in your genomic data.
-              </li>
-              <li>
-                Nutree also constructs a 3D tree animation, that serves as your unique nutritional fingerprint, in order to help you visualize your natural nutritional needs.
-              </li>
-              <li>
-                The tree consists of 11 branches of various sizes, with branch sizes representative of your predicted micronutrient levels.
-              </li>
+              <li>Using your personal nutrional data, NuTree suggests recipes with high levels of the micronutrients your body craves most, based on predictative factors in your genomic data.</li>
+              <li>Nutree also constructs a 3D tree animation, that serves as your unique nutritional fingerprint, in order to help you visualize your natural nutritional needs.</li>
+              <li>The tree consists of 11 branches of various sizes, with branch sizes representative of your predicted micronutrient levels.</li>
             </ul>
-
           </div>
         </section>
 
-        <section className="style3" id="three">
+        <section className="style 3" id="how">
           <div className="three-content">
             <header>
               <h1>
                 How does Nutree work?
               </h1>
             </header>
-            <br />
             <h2>
               Nutritional data fingerprint:
             </h2>
-            <br />
             <ul>
               <li>You upload your 23andme, ancestory.com, or MyHeritage data through genomeLink and connect your account through this application.</li>
-              <br />
-              <li>
-                Nutree uses your overall genomic nutritional profile in order to build a unique 3D visualization of this data that you can then share with friends
-              </li>
-              <br />
-              <li>
-                Larger branches near the bottom of the tree are representative of micronutrients that are predicted to be naturally abundant in your bloodstream.
-              </li>
-              <br />
-              <li>
-                Smaller branches are representative of micronutrients that your body may have naturally lower blood serum levels for.
-              </li>
-              <br />
-              <li>
-                Your micronutrient tree visualization is 100% unique to your genetic information and as a result is as personalized to you as your fingerprint!
-              </li>
-              <br />
+              <li>Nutree uses your overall genomic nutritional profile in order to build a unique 3D visualization of this data that you can then share with friends</li>
+              <li> Larger branches near the bottom of the tree are representative of micronutrients that are predicted to be naturally abundant in your bloodstream.</li>
+              <li>Smaller branches are representative of micronutrients that your body may have naturally lower blood serum levels for.</li>
+              <li> Your micronutrient tree visualization is 100% unique to your genetic information and as a result is as personalized to you as your fingerprint!</li>
             </ul>
             <h2>
               Recipe Suggestions:
             </h2>
-            <br />
             <ul>
-              <li>
-                Based on genetic markers predictative of natural blood serum levels for 11 essential micronutrients...
-              </li>
-              <br />
-              <li>
-                Nutree suggests recipes that are high in the three micronutrients that your genetic data suggests your body might need the most of.
-              </li>
-              <br />
-              <li>
-                Custom search features are availible through Edamam recipe search API to help you filter out potential allergens and other foods that are restricted from your diet.
-              </li>
-              <br />
+              <li>Based on genetic markers predictative of natural blood serum levels for 11 essential micronutrients...</li>
+              <li>Nutree suggests recipes that are high in the three micronutrients that your genetic data suggests your body might need the most of.</li>
+              <li>Custom search features are availible through Edamam recipe search API to help you filter out potential allergens and other foods that are restricted from your diet.</li>
             </ul>
           </div>
         </section>
 
-        <section className="style2" id="two">
+        <section className="style 2" id="science">
           <div className="two-content">
-            <header>
-              <h1>
-          What science is this project based upon?
-              </h1>
-            </header>
-            <br />
+            <h1>What science is this project based upon?</h1>
             <ul className="intro">
-
-              <li>            The scientific connections between genomic data and nutrient levels used in this application are based upon genome-wide association studies.
-              </li>
-              <br />
-              <li>
-            By analyzing genomic data from large samples of the population, researchers have discovered links between certain genetic markers and serum nutrient levels.
-              </li>
-              <br />
-              <li>
-            Be aware that genetic markers predictative of blood serum nutrient levels are by no means definitively reflective of your current micronutrient levels.
-              </li>
-              <br />
-              <li>
-            The human body is a complicated biochemical system and many other factors such as diet and exercise also contribute to a person's serum nutrient levels at any given time.
-              </li>
-              <br />
-              <li>
-            Nutree is meant to provide insight into what types of nutrients your body might require higher levels of, while suggesting foods that may help your body fulfill nutritional requirements.
-              </li>
-              <br />
-              <li>
-            Information and statements regarding dietary supplements have not been evaluated by the Food and Drug Administration and are not intended to diagnose, treat, cure, or prevent any disease.
-              </li>
+              <li>The scientific connections between genomic data and nutrient levels used in this application are based upon genome-wide association studies.</li>
+              <li>By analyzing genomic data from large samples of the population, researchers have discovered links between certain genetic markers and serum nutrient levels.</li>
+              <li> Be aware that genetic markers predictative of blood serum nutrient levels are by no means definitively reflective of your current micronutrient levels.</li>
+              <li>The human body is a complicated biochemical system and many other factors such as diet and exercise also contribute to a person's serum nutrient levels at any given time.</li>
+              <li>Nutree is meant to provide insight into what types of nutrients your body might require higher levels of, while suggesting foods that may help your body fulfill nutritional requirements.</li>
+              <li>Information and statements regarding dietary supplements have not been evaluated by the Food and Drug Administration and are not intended to diagnose, treat, cure, or prevent any disease.</li>
             </ul>
           </div>
         </section>
